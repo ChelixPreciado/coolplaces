231dea58c9d6824afcb6a839a1aa73eed1b2e7dc
Jose Luis Preciado <j.luis.preciado.lopez@gmail.com>
2016-07-14 19:18:01 -0500

    agregando proyecto

---
+++ CoolPlaces/CoolApp/app/src/main/java/interware/coolapp/main/AppController.java
@@ -0,0 +1,15 @@
+package interware.coolapp.main;
+
+import android.app.Application;
+
+
+/**
+ * Created by chelixpreciado on 7/13/16.
+ */
+public class AppController extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ CoolPlaces/CoolApp/app/src/main/java/interware/coolapp/ui/Register/RegisterActivity.java
@@ -0,0 +1,178 @@
+package interware.coolapp.ui.Register;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+
+import interware.coolapp.R;
+import interware.coolapp.ui.Loader.Loader;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A register screen that offers login via email/password.
+ */
+public class RegisterActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
+
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private View mLoginFormView;
+
+    private FirebaseAuth mAuth;
+    private Loader loader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        // Set up the login form.
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button register = (Button) findViewById(R.id.btn_register);
+        register.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(this);
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            registerUser(email, password);
+        }
+    }
+
+    private void registerUser(String email, String password){
+        showLoader(true);
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                showLoader(false);
+                Log.i("Chelix", "createUserWithEmailAndPassword:onComplete: " + task.isSuccessful());
+                if (task.isSuccessful())
+                    Log.i("Chelix", "Created user: " + task.getResult().getUser().toString());
+                else
+                    Log.i("Chelix", "Usuario no creado: " + task.getException().getMessage());
+            }
+        });
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            // User is signed in
+            Log.d("Chelix", "onAuthStateChanged:signed_in:" + user.getUid());
+        } else {
+            // User is signed out
+            Log.d("Chelis", "onAuthStateChanged:signed_out");
+        }
+    }
+
+    private void showLoader(boolean show){
+        if (loader==null)
+            loader = Loader.newInstance();
+        if (show)
+            loader.show(getFragmentManager(), "showLoader");
+        else
+            loader.dismiss();
+    }
+}
+
